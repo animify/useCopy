608d2c9abc15ce9d4cd5eec24289a5bf8ee17b73
Stefan Mansson <stefan.aotik@gmail.com>
2019-05-13 19:35:42 +0300

    chore: more config files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import copy from 'copy-to-clipboard';
+import copyToClipboard from 'copy-to-clipboard';
 import { useState, useRef, useEffect, useCallback } from 'react';
 
 export default function useCopy(str: string): [boolean, () => void, (value: boolean) => void] {
     const copyableString = useRef(str);
     const [copied, setCopied] = useState(false);
     const copyAction = useCallback(() => {
-        const copiedString = copy(copyableString.current);
+        const copiedString = copyToClipboard(copyableString.current);
         setCopied(copiedString);
     }, [copyableString]);
 
